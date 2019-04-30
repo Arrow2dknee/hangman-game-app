92bb1f3b911082a576c3f30a337fb6f1ae052f20
Antony Roshan <antrosh92@gmail.com>
2019-04-30 12:14:34 +0530

    edit api call to support both http and https

--- src/requests.js
+++ src/requests.js
@@ -1,14 +1,15 @@
-
 // using async-await
 async function getPuzzle(wordCount) {
-    
-    let response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`, {});
-    if (response.status === 200) {
-        const data = await response.json();
-        return data.puzzle;
-    } else {
-        throw new Error(response.statusText);
-    }
+  let response = await fetch(
+    `//puzzle.mead.io/puzzle?wordCount=${wordCount}`,
+    {}
+  );
+  if (response.status === 200) {
+    const data = await response.json();
+    return data.puzzle;
+  } else {
+    throw new Error(response.statusText);
+  }
 }
 
 export { getPuzzle as default };
